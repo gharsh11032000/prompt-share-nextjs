938c6b6f1ee213c9aceafc0594e4b2338ea68a5a
harsh <gharsh11032000@gmail.com>
2023-05-31 18:13:31 +0530

    Changing google path

--- app/components/ButtonGoogle.js
+++ app/components/ButtonGoogle.js
@@ -4,7 +4,7 @@ import { API_URL } from "../config";
 
 function ButtonGoogle({ children }) {
   return (
-    <Link href={`${API_URL}/connect/google/callback`}>
+    <Link href={`${API_URL}/connect/google`}>
       <button className="btn btn-primary">
         <FaGoogle className="mr-2" />
         {children}
