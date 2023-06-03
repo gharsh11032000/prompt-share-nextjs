cbdc8a485bcdefea514431095372e0724ec3bede
harsh <gharsh11032000@gmail.com>
2023-06-03 17:05:22 +0530

    changing page limit for prompts

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:1337/api";
 
-export const PER_PAGE = 2;
+export const PER_PAGE = 15;
 
-export const HOMEPAGE_PAGINATION_LIMIT = 6;
+export const HOMEPAGE_PAGINATION_LIMIT = 10;
